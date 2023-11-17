32174c9f8a808e04773990ea92a3e54cb2a26b1e
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-17 13:14:55 +0100

    [ADD]

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+// Esercizio 1
 let number = 100;
 console.log(number);
+
+// Esercizio 2
+const PI = 3.1416;
+console.log(PI);
+
+// Esercizio 2/B
+// ERRORE: PI = 3.14;
+// console.log(PI);
+
+// Esercizio 2/C
+let radius = 8;
+let circle = radius * 2 * PI;
+console.log(circle);
+
+// Esercizio 3
+let name = "Carlo";
+name = "Marco";
+console.log(name);
