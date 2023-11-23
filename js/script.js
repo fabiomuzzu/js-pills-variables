807ee35d98d03e427d8725f478ac6e7b32739653
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 10:12:29 +0100

    lilpush

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 // Esercizio 1
-let number = 100;
-console.log(number);
+// let number = 100;
+// console.log(number);
 
 // Esercizio 2
-const PI = 3.1416;
-console.log(PI);
+// const PI = 3.1416;
+// console.log(PI);
 
 // Esercizio 2/B
 // ERRORE: PI = 3.14;
 // console.log(PI);
 
 // Esercizio 2/C
-let radius = 8;
-let circle = radius * 2 * PI;
-console.log(circle);
+// let radius = 8;
+// let circle = radius * 2 * PI;
+// console.log(circle);
 
 // Esercizio 3
-let name = "Carlo";
-name = "Marco";
-console.log(name);
+// let name = "Carlo";
+// name = "Marco";
+// console.log(name);
 
 // Esercizio 4
-let greet = "Ciao, Mondo!";
-console.log(greet);
+// let greet = "Ciao, Mondo!";
+// console.log(greet);
 
 // Esercizio 4/b
 // greet = "Come stai, Mondo?";
 // console.log(greet);
 
 // Esercizio 4/c
-greet = greet + " " + "Come stai?"
-console.log(greet);
+// greet = greet + " " + "Come stai?"
+// console.log(greet);
 
 
 // Esercizio 5
-let num =  9;
-let num2 = 10;
+// let num =  9;
+// let num2 = 10;
 
 // let comparison;
 
@@ -61,14 +61,18 @@ let num2 = 10;
 // }
 // console.log(comparison);
 
-let pippo = 1;
-let comparison;
+// let pippo = 1;
+// let comparison;
 
-if (pippo == '1'){
-    comparison = true;
-}
-else{
-    comparison = false;
-}
-console.log(comparison);
+// if (pippo == '1'){
+//     comparison = true;
+// }
+// else{
+//     comparison = false;
+// }
+// console.log(comparison);
 
+for (let i=1; i<=42; i++){
+    console.log(i)
+    
+}
